85192b6cab8fac9254ccd79bfc04f7478c7c288f
siddardhakavuri <siddardhakavuri@protonmail.com>
2026-02-26 16:39:46 -0700

    Fixed astro transition reset when user reclicks in the middle of animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import "./App.css";
-import { motion } from "motion/react";
 import Astro from "./components/Astro";
 
 function App() {
   return (
     <>
-      <motion.h1
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ ease: [0, 0, 0.5, 1], duration: 1.4, repeat: Infinity }}
-      >
-        {/* Coming soon */}
-      </motion.h1>
       <Astro />
     </>
   );
--- src/components/Astro.tsx
+++ src/components/Astro.tsx
@@ -5,42 +5,50 @@ import { useState } from "react";
 
 function Astro() {
   let [booster, setBooster] = useState(true);
+  let in_transition: boolean = false;
 
   let astro_click = () => {
-    //TODO: Implement booster switching on and off transition
+    //TODO: Implement quick sequence of booster on and off to give illusion of astro's disapproval
     setBooster(!booster);
   };
 
   return (
     <span
       onClick={() => {
-        astro_click();
+        !in_transition && astro_click();
       }}
     >
-      {booster ? (
-        <motion.img
-          initial={{ y: "100vh" }}
-          animate={{ y: 0 }}
-          transition={{
-            ease: "easeOut",
-            duration: 2,
-          }}
-          width={"200px"}
-          src={astro_icon}
-          alt="astro"
-        />
-      ) : (
-        <motion.img
-          animate={{ y: "100vh" }}
-          onAnimationComplete={() => {
-            setBooster(!booster);
-          }}
-          transition={{ duration: 0.6 }}
-          width={"200px"}
-          src={astro_fall_icon}
-          alt="astro_falling"
-        />
-      )}
+      {booster
+        ? !in_transition &&
+          (in_transition = true) && (
+            <motion.img
+              initial={{ y: "100vh" }}
+              animate={{ y: 0 }}
+              onAnimationComplete={() => {
+                in_transition = false;
+              }}
+              transition={{
+                ease: "easeOut",
+                duration: 2,
+              }}
+              width={"200px"}
+              src={astro_icon}
+              alt="astro"
+            />
+          )
+        : !in_transition && (
+            <motion.img
+              animate={{ y: "100vh" }}
+              onAnimationComplete={() => {
+                in_transition = false;
+                setBooster(!booster);
+              }}
+              transition={{ duration: 0.6 }}
+              width={"200px"}
+              src={astro_fall_icon}
+              alt="astro_falling"
+            />
+          )}
     </span>
   );
 }
