fc5661616feca520cd3d668b27e42378a0603abe
siddardhakavuri <siddardhakavuri@protonmail.com>
2026-03-04 18:04:56 -0700

    Included project details

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,32 +2,32 @@ import { motion } from "motion/react";
 
 const projects = [
   {
-    title: "Project 1",
+    title: "Neural Mapping of Gene-Sequence Risk Factors",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum rem, esse odit reprehenderit ex enim veniam similique officiis eius cum ullam velit alias. Labore voluptate repellat explicabo ipsum iste!",
-    tags: ["React", "Node.js", "Stripe"],
-    link: "#",
+      "Deep learning framework designed to predict the likelihood of cancer by analyzing the complex, non-linear associations between specific gene sequences.",
+    tags: [
+      "Neural Networks",
+      "Python",
+      "TensorFlow",
+      "Scikit-learn",
+      "Pandas",
+      "Numpy",
+    ],
+    link: "https://github.com/siddardhakavuri/Modelling-Associations",
   },
   {
-    title: "Project 2",
+    title: "Portfolio Website",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum rem, esse odit reprehenderit ex enim veniam similique officiis eius cum ullam velit alias. Labore voluptate repellat explicabo ipsum iste!",
-    tags: ["Next.js", "OpenAI", "Tailwind"],
-    link: "#",
-  },
-  {
-    title: "Project 3",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum rem, esse odit reprehenderit ex enim veniam similique officiis eius cum ullam velit alias. Labore voluptate repellat explicabo ipsum iste!",
-    tags: ["TypeScript", "Vite", "Framer Motion"],
-    link: "#",
-  },
-  {
-    title: "Project 4",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum rem, esse odit reprehenderit ex enim veniam similique officiis eius cum ullam velit alias. Labore voluptate repellat explicabo ipsum iste!",
-    tags: ["React", "Firebase", "Redux"],
-    link: "#",
+      "A high-performance showcase built with type-safe, component-driven architecture and hardware-accelerated animations and page transitions.",
+    tags: [
+      "TypeScript",
+      "Tailwind",
+      "Framer Motion",
+      "React",
+      "Vite",
+      "Cloudflare",
+    ],
+    link: "https://github.com/siddardhakavuri/Portfolio",
   },
 ];
 
@@ -37,26 +37,28 @@ const Projects = () => {
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
+        transition={{ duration: 1 }}
         viewport={{ once: true }}
       >
         <h2 className="text-4xl font-bold mb-12 bg-linear-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent inline-block">
           Projects
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {projects.map((project, index) => (
             <motion.a
-              href={"#projects"}
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
               key={index}
               initial={{ opacity: 0, scale: 0.95 }}
               whileInView={{ opacity: 1, scale: 1 }}
               whileHover={{ y: -5, transition: { duration: 0.1 } }}
               transition={{ duration: 0.2 }}
               viewport={{ once: true }}
-              className="group block p-8 rounded-2xl bg-white/5 border border-white/10 hover:bg-white/10 transition-colors"
+              className="group block p-8 rounded-2xl bg-white/7 border border-white/15 hover:bg-white/12 transition-colors"
             >
-              <h3 className="text-2xl font-bold text-white mb-3 group-hover:text-cyan-400 transition-colors">
+              <h3 className="text-2xl font-bold text-white mb-3 group-hover:text-cyan-400 transition-colors min-h-16 flex items-center">
                 {project.title}
               </h3>
               <p className="text-gray-400 my-10 leading-relaxed">
