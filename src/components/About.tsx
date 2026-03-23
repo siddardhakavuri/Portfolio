d58b040ebac23c0dc7beb42a1ec248817d69fecc
siddardhakavuri <siddardhakavuri@protonmail.com>
2026-03-22 19:21:55 -0700

    Changed name visibility

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,7 +17,7 @@ const About = () => {
             <span className="absolute bottom-full sm:-left-10 left-0 mb-1 bg-linear-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent text-lg">
               Hey I'm
             </span>
-            <h1 className="text-5xl font-bold bg-linear-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent">
+            <h1 className="text-6xl font-bold bg-linear-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent">
               SIDDARDHA KAVURI
             </h1>
           </div>
